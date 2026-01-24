4e1edd3ab8a3ad8aa504dae4bdef167a33b5bfbb
Tobias <tobias.chlapek@appitect.eu>
2026-01-24 18:01:24 +0100

    EKF

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -2,7 +2,12 @@ import type { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: [{ userAgent: "*", allow: "/" }],
+    rules: [
+      {
+        userAgent: "*",
+        allow: "/",
+      },
+    ],
     sitemap: "https://flekcuts.cz/sitemap.xml",
   };
 }
--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,5 +1,10 @@
 import type { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  return [{ url: "https://flekcuts.cz/", lastModified: new Date() }];
+  return [
+    {
+      url: "https://flekcuts.cz/",
+      lastModified: new Date(),
+    },
+  ];
 }
